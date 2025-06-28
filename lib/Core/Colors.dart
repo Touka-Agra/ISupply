60351ee5e5250550d94cf7d62033df07d8e28a25
Touka-Agra <touka.agra@gmail.com>
2025-06-28 12:29:35 +0300

    Colors.dart

---
+++ lib/Core/Colors.dart
@@ -0,0 +1,13 @@
+
+import 'dart:ui';
+
+class MyColors{
+  static Color mainColor = Color(0XFF1F4698);
+  static Color secondaryColor = Color(0XFF009FE3);
+  static Color lightColor = Color(0XFF83D0F5);
+
+  static Color highlightColor = Color(0XFFF5D061);
+  static Color greenColor = Color(0XFF79C6C0);
+
+  static Color white = Color(0XFFFFFFFF);
+}
